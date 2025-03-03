ca41db4ff2c8043535aa42659da5d370119be48d
mubashirmi <111482469+mubashirmi@users.noreply.github.com>
2025-03-03 14:53:37 +0500

    final push today

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import Tab6FormPage from './pages/Tab6FormPage'
 import Tab6MainPage from './pages/Tab6MainPage'
 import { Routes ,Route } from 'react-router-dom'
 import PiebackTabCalculator from './pages/PiebackTabCalculator'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
+    <>
+    <Navbar />
     <Routes>
       <Route path='/' element={<Tab1MainPage />} />
       <Route path='/tab1form' element={<Tab1FormPage />} />
@@ -14,6 +17,7 @@ const App = () => {
       <Route path='/businesscardform/:cardId' element={<Tab6FormPage />} />
       <Route path='/piebackcalculator' element={<PiebackTabCalculator />} />
     </Routes>
+    </>
   )
 }
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Navbar = () => {
+    const navigate = useNavigate();
+  return (
+    <div className='flex pt-3 pb-4 justify-center gap-x-5 bg-[#222]'>
+        <button onClick={()=>navigate('/')} className='py-2 px-5 text-xl font-medium cursor-pointer rounded-xl text-[#f4f4f4] border-2 border-[#f4f4f4] '>Tab 1(Flyer)</button>      
+        <button onClick={()=>navigate('/businesscard')} className='py-2 px-5 text-xl font-medium cursor-pointer rounded-xl text-[#f4f4f4] border-2 border-[#f4f4f4] '>Tab 2(Business Card)</button>      
+        <button onClick={()=>navigate('/piebackcalculator')} className='py-2 px-5 text-xl font-medium cursor-pointer rounded-xl text-[#f4f4f4] border-2 border-[#f4f4f4] '>Tab 3(Calculator)</button>      
+    </div>
+  )
+}
+
+export default Navbar
