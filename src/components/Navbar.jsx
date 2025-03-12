64b6951f441a12226daf17f01e2a2a0ce32ea983
mubashirmi <111482469+mubashirmi@users.noreply.github.com>
2025-03-12 22:20:35 +0500

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,18 @@ import { useNavigate } from 'react-router-dom'
 const Navbar = () => {
     const navigate = useNavigate();
   return (
-    <div className='flex pt-3 pb-4 justify-center gap-x-4 bg-[#222]'>
-        <button onClick={()=>navigate('/')} className='py-2 px-5 text-lg font-medium cursor-pointer rounded-lg text-[#f4f4f4] border-2 border-[#f4f4f4] '>Flyer</button>      
-        <button onClick={()=>navigate('/businesscard')} className='py-2 px-5 text-lg font-medium cursor-pointer rounded-lg text-[#f4f4f4] border-2 border-[#f4f4f4] '>Business Card</button>      
-        <button onClick={()=>navigate('/sliceOfTheMarket')} className='py-2 px-5 text-lg font-medium cursor-pointer rounded-lg text-[#f4f4f4] border-2 border-[#f4f4f4] '>Slice of the Market</button>      
-        <button onClick={()=>navigate('/chargebackRiskAnalysis')} className='py-2 px-5 text-lg font-medium cursor-pointer rounded-lg text-[#f4f4f4] border-2 border-[#f4f4f4] '>Chargeback Risk Analysis</button>      
-        <button onClick={()=>navigate('/loanFinancialAnalysis')} className='py-2 px-5 text-lg font-medium cursor-pointer rounded-lg text-[#f4f4f4] border-2 border-[#f4f4f4] '>Loan Financial Analysis</button>      
-        <button onClick={()=>navigate('/piebackcalculator')} className='py-2 px-5 text-lg font-medium cursor-pointer rounded-lg text-[#f4f4f4] border-2 border-[#f4f4f4] '>Pie back Calculator</button>      
-        <button onClick={()=>navigate('/tab7form/tab7')} className='py-2 px-5 text-lg font-medium cursor-pointer rounded-lg text-[#f4f4f4] border-2 border-[#f4f4f4] '>Schedulle a Meeting</button>      
-        <button onClick={()=>navigate('/pie-proshop')} className='py-2 px-5 text-lg font-medium cursor-pointer rounded-lg text-[#f4f4f4] border-2 border-[#f4f4f4] '>Pie ProShop</button>      
+    <div className='flex pt-3 pb-4 justify-between  text-black/80 font-medium text-base items-center max-w-[1440px] mx-auto'>
+        <div className='w-11'>
+          <img className='' src="Logo.png" alt="Logo" />
+        </div>
+        <button onClick={()=>navigate('/')} className=' cursor-pointer rounded-lg '>Flyer</button>      
+        <button onClick={()=>navigate('/businesscard')} className=' cursor-pointer rounded-lg '>Business Card</button>      
+        <button onClick={()=>navigate('/sliceOfTheMarket')} className=' cursor-pointer rounded-lg '>Slice of the Market</button>      
+        <button onClick={()=>navigate('/chargebackRiskAnalysis')} className=' cursor-pointer rounded-lg '>Chargeback Risk Analysis</button>      
+        <button onClick={()=>navigate('/loanFinancialAnalysis')} className=' cursor-pointer rounded-lg '>Loan Financial Analysis</button>      
+        <button onClick={()=>navigate('/piebackcalculator')} className=' cursor-pointer rounded-lg '>Pie back Calculator</button>      
+        <button onClick={()=>navigate('/tab7form/tab7')} className=' cursor-pointer rounded-lg '>Schedulle a Meeting</button>      
+        <button onClick={()=>navigate('/pie-proshop')} className=' cursor-pointer rounded-lg '>Pie ProShop</button>      
     </div>
   )
 }
