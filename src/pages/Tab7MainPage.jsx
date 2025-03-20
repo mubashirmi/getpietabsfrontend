81e237268349f0ea8edc1f1e6f37dc2078ec56ea
mubashirmi <111482469+mubashirmi@users.noreply.github.com>
2025-03-20 23:48:47 +0500

    tab 7 new design done

--- src/pages/Tab7MainPage.jsx
+++ src/pages/Tab7MainPage.jsx
@@ -22,7 +22,7 @@ const Tab7MainPage = () => {
     });
   };
 
-  const handleSubmitSecondForm = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     // Validate the second form
     if (
@@ -45,142 +45,143 @@ const Tab7MainPage = () => {
   };
 
   return (
-    <div className="max-w-2xl mx-auto p-5 bg-white shadow-lg rounded-lg my-6 border-2 border-slate-200">
-      <form onSubmit={handleSubmitSecondForm}>
-        <h2 className="text-3xl font-semibold mb-4 text-center">Tab7 Form</h2>
+    <div className="min-h-[calc(100vh-72px)] flex justify-center items-center bg-gradient-to-r from-[#0071E3] to-[#002F5F] w-full px-4 sm:px-8 py-10">
+      <div className="max-w-[1440px] mx-auto w-[99%] flex justify-center items-center gap-x-9 ">
 
-        <div className="mb-4">
-          <label htmlFor="processor" className="block text-sm font-medium text-gray-700">Who do you process with now?</label>
-          <input
-            type="text"
-            id="processor"
-            name="processor"
-            value={formData.processor}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
+        <div className="w-[886px] bg-white px-10 py-12 h-full rounded-[25px] shadow-2xl shadow-black/10">
+          <h3 className="text-4xl text-[#1E1E1E] font-semibold uppercase text-center mb-3 tracking-wide">
+          Merchant Analysis
+          </h3>
+          <form onSubmit={handleSubmit}>
+              <>
+                <div className="mb-4 flex flex-col gap-1">
+                  <label htmlFor="processor" className="text-base font-light">
+                  Who do you process with now?
+                  </label>
+                  <input
+                    type="text"
+                    id="processor"
+                    name="processor"
+                    value={formData.processor}
+                    onChange={handleChange}
+                    className="text-lg border-[1px] font-medium border-[#D6D6D6] text-[#333] outline-none rounded-[10px] p-2.5"
+                  />
+                </div>
+                <div className="mb-4 flex flex-col gap-1">
+                  <label htmlFor="businessDuration" className="text-base font-light">
+                  How long have you been in business?
+                  </label>
+                  <input
+                    type="text"
+                    id="businessDuration"
+                    name="businessDuration"
+                    value={formData.businessDuration}
+                    onChange={handleChange}
+                    className="text-lg border-[1px] font-medium border-[#D6D6D6] text-[#333] outline-none rounded-[10px] p-2.5"
+                  />
+                </div>
+
+                <div className="mb-4 flex flex-col gap-1">
+                  <label htmlFor="previousProcessor" className="text-base font-light">
+                    Have you used another processor before?
+                  </label>
+                  <input
+                    type="text"
+                    id="previousProcessor"
+                    name="previousProcessor"
+                    value={formData.previousProcessor}
+                    onChange={handleChange}
+                    className="text-lg border-[1px] font-medium border-[#D6D6D6] text-[#333] outline-none rounded-[10px] p-2.5"
+                  />
+                </div>
+                <div className="mb-4 flex flex-col gap-1">
+                  <label htmlFor="priceIncrease" className="text-base font-light">
+                  Have you raised your pricing inside your business in the past? What happened when you did?
+                  </label>
+                  <input
+                    type="text"
+                    id="priceIncrease"
+                    name="priceIncrease"
+                    value={formData.priceIncrease}
+                    onChange={handleChange}
+                    className="text-lg border-[1px] font-medium border-[#D6D6D6] text-[#333] outline-none rounded-[10px] p-2.5"
+                  />
+                </div>
+                <div className="mb-4 flex flex-col gap-1">
+                  <label htmlFor="growthPlans" className="text-base font-light">
+                  What growth plans do you have for your business over the next 5 years?
+                  </label>
+                  <input
+                    type="tel"
+                    id="growthPlans"
+                    name="growthPlans"
+                    value={formData.growthPlans}
+                    onChange={handleChange}
+                    className="text-lg border-[1px] font-medium border-[#D6D6D6] text-[#333] outline-none rounded-[10px] p-2.5"
+                  />
+                </div>
+                <div className="mb-4 flex flex-col gap-1">
+                  <label htmlFor="profitMargin" className="text-base font-light">
+                    What is your average profit margin across the business?
+                  </label>
+                  <input
+                    type="text"
+                    id="profitMargin"
+                    name="profitMargin"
+                    value={formData.profitMargin}
+                    onChange={handleChange}
+                    className="text-lg border-[1px] font-medium border-[#D6D6D6] text-[#333] outline-none rounded-[10px] p-2.5"
+                  />
+                </div>
+                <div className="mb-4 flex flex-col gap-1">
+                  <label htmlFor="ticketPrice" className="text-base font-light">
+                    What's your average ticket price?
+                  </label>
+                  <input
+                    type="text"
+                    id="ticketPrice"
+                    name="ticketPrice"
+                    value={formData.ticketPrice}
+                    onChange={handleChange}
+                    className="text-lg border-[1px] font-medium border-[#D6D6D6] text-[#333] outline-none rounded-[10px] p-2.5"
+                  />
+                </div>
+                <div className="mb-4 flex flex-col gap-1">
+                  <label htmlFor="foodDelivery" className="text-base font-light">
+                  Do you utilize any food delivery services? (If a restaurant)
+                  </label>
+                  <input
+                    type="text"
+                    id="foodDelivery"
+                    name="foodDelivery"
+                    value={formData.foodDelivery}
+                    onChange={handleChange}
+                    className="text-lg border-[1px] font-medium border-[#D6D6D6] text-[#333] outline-none rounded-[10px] p-2.5"
+                  />
+                </div>
+                <div className="mb-4 flex flex-col gap-1">
+                  <label htmlFor="costIncreasePlans" className="text-base font-light">
+                  What plans or goals do you have to increase profit or decrease cost?
+                  </label>
+                  <input
+                    type="text"
+                    id="costIncreasePlans"
+                    name="costIncreasePlans"
+                    value={formData.costIncreasePlans}
+                    onChange={handleChange}
+                    className="text-lg border-[1px] font-medium border-[#D6D6D6] text-[#333] outline-none rounded-[10px] p-2.5"
+                  />
+                </div>
+                <button className='text-white w-full font-medium text-xl bg-[#0071E3] py-2.5 px-8 rounded-[10px] cursor-pointer  hover:shadow-blue-500/20 duration-300 transition-all ease-in-out hover:shadow-xl'>
+                  Next
+                </button>
+              </>
+
+
+
+          </form>
         </div>
-
-        <div className="mb-4">
-          <label htmlFor="businessDuration" className="block text-sm font-medium text-gray-700">How long have you been in business?</label>
-          <input
-            type="text"
-            id="businessDuration"
-            name="businessDuration"
-            value={formData.businessDuration}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="previousProcessor" className="block text-sm font-medium text-gray-700">Have you used another processor before?</label>
-          <input
-            type="text"
-            id="previousProcessor"
-            name="previousProcessor"
-            value={formData.previousProcessor}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="volume" className="block text-sm font-medium text-gray-700">How much volume in credit and debit do you do?</label>
-          <input
-            type="text"
-            id="volume"
-            name="volume"
-            value={formData.volume}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="priceIncrease" className="block text-sm font-medium text-gray-700">Have you raised your pricing inside your business in the past? What happened when you did?</label>
-          <input
-            type="text"
-            id="priceIncrease"
-            name="priceIncrease"
-            value={formData.priceIncrease}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="growthPlans" className="block text-sm font-medium text-gray-700">What growth plans do you have for your business over the next 5 years?</label>
-          <input
-            type="text"
-            id="growthPlans"
-            name="growthPlans"
-            value={formData.growthPlans}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="profitMargin" className="block text-sm font-medium text-gray-700">What is your average profit margin across the business?</label>
-          <input
-            type="text"
-            id="profitMargin"
-            name="profitMargin"
-            value={formData.profitMargin}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="ticketPrice" className="block text-sm font-medium text-gray-700">What's your average ticket price?</label>
-          <input
-            type="text"
-            id="ticketPrice"
-            name="ticketPrice"
-            value={formData.ticketPrice}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="foodDelivery" className="block text-sm font-medium text-gray-700">Do you utilize any food delivery services? (If a restaurant)</label>
-          <input
-            type="text"
-            id="foodDelivery"
-            name="foodDelivery"
-            value={formData.foodDelivery}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="costIncreasePlans" className="block text-sm font-medium text-gray-700">What plans or goals do you have to increase profit or decrease cost?</label>
-          <input
-            type="text"
-            id="costIncreasePlans"
-            name="costIncreasePlans"
-            value={formData.costIncreasePlans}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
-
-        <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-md">Submit</button>
-      </form>
+      </div>
     </div>
   );
 };
