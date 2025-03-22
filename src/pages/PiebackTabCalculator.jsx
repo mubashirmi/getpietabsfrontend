7763c2dc13c248c2abf52ec5966ab106bb113b56
mubashirmi <111482469+mubashirmi@users.noreply.github.com>
2025-03-22 14:37:45 +0500

    pie back calculator half

--- src/pages/PiebackTabCalculator.jsx
+++ src/pages/PiebackTabCalculator.jsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import { LuCalculator , LuPhoneCall} from "react-icons/lu";
+import { LuCalculator, LuPhoneCall } from "react-icons/lu";
 import { Doughnut } from "react-chartjs-2";
 import { Chart as ChartJS, Title, Tooltip, ArcElement, Legend } from "chart.js";
 import { FaLocationArrow } from "react-icons/fa6";
@@ -108,40 +108,36 @@ const PiebackTabCalculator = () => {
         setIsYearly(e.target.value === "Yearly");
     };
 
-  const handleCreateBusinessCard = async () => {
-    try {
-      const element = calculatorRef.current;
-      if (!element) return;
-  
-      const dataUrl = await domtoimage.toPng(element);
-      const response = await fetch(dataUrl);
-      const blob = await response.blob();
-      const formData = new FormData();
-      formData.append('image', blob, 'business_card.png');
-      navigate(`/general-info-form/${"1"}/piebackCalculator`);
+    const handleCreateBusinessCard = async () => {
+        try {
+            const element = calculatorRef.current;
+            if (!element) return;
 
-      await fetch('http://localhost:4000/api/tab6', {
-        method: 'POST',
-        body: formData,
-      });  
-    } catch (error) {
-      console.error('Error capturing and sending image:', error);
-    }
-  };
-      
+            const dataUrl = await domtoimage.toPng(element);
+            const response = await fetch(dataUrl);
+            const blob = await response.blob();
+            const formData = new FormData();
+            formData.append('image', blob, 'business_card.png');
+            navigate(`/general-info-form/${"1"}/piebackCalculator`);
+            await fetch('http://localhost:4000/api/tab6', {
+                method: 'POST',
+                body: formData,
+            });
+        } catch (error) {
+            console.error('Error capturing and sending image:', error);
+        }
+    };
+
 
     return (
-        <div className="min-h-[100vh] bg-gradient-to-r from-[#0071E3] to-[#002F5F] w-full flex flex-col justify-center items-center pt-20 pb-10">
-            <div className="w-6xl bg-white rounded-4xl border-[3px] border-blue-800/60 px-4 pt-16 pb-10 relative" ref={calculatorRef}>
-                <div className='absolute top-0 bg-blue-100 text-blue-800/80 translate-y-[-50%] left-[50%] translate-x-[-50%] w-[7rem] h-[7rem] flex justify-center items-center rounded-full border-[3px] border-blue-800/60 shadow-lg shadow-blue-100'>
-                    <LuCalculator size={50} />
-                </div>
-                <h3 className='text-[40px] font-semibold text-[#444] text-center font-sans mb-2'> PIE BACK CALCULATOR</h3>
-                <div className='flex justify-between gap-4'>
-                <div className='w-[62%] pl-3'>
+        <div className="min-h-[100vh] bg-gradient-to-r from-[#4686BC] to-[#62956A] w-full flex flex-col justify-center items-center pt-20 pb-10">
+            <div className="w-[1211px] bg-white rounded-[25px] p-[30px] relative" ref={calculatorRef}>
+                <h3 className='text-[32px] font-bold text-center mb-5'> PIE BACK CALCULATOR</h3>
+                <div className='flex justify-between gap-3'> 
+                    <div className='w-[50%] pl-3'>
                         {/* Referral Account Volume */}
                         <div className="flex flex-col space-x-4 mb-3">
-                            <label htmlFor="referralAccountVolume" className="text-lg font-medium text-gray-700" >
+                            <label htmlFor="referralAccountVolume" className="text-xl font-medium" >
                                 Average Referral Account Volume
                             </label>
                             <input
@@ -153,7 +149,7 @@ const PiebackTabCalculator = () => {
                                 onChange={handleReferralVolumeRangeChange}
                             />
                             <input
-                                className='border-2 font-medium text-[#444] bg-blue-100/70 border-blue-200 outline-none py-1 px-2 text-lg mt-1 rounded-md'
+                                className='border-[1px] font-medium text-[#333] border-[#D6D6D6] outline-none p-[10px] text-lg mt-1 rounded-[10px]'
                                 type="text"
                                 id="referralAccountVolumeText"
                                 value={referralAccountVolume}
@@ -164,7 +160,7 @@ const PiebackTabCalculator = () => {
                         <div className="flex flex-col space-x-4 mb-3">
                             <label
                                 htmlFor="numberOfReferrals"
-                                className="text-lg font-medium text-gray-700"
+                                className="text-xl font-medium"
                             >
                                 Number of Referred Merchants
                             </label>
@@ -177,7 +173,7 @@ const PiebackTabCalculator = () => {
                                 onChange={handleNumberOfReferralsRangeChange}
                             />
                             <input
-                                className='border-2 font-medium text-[#444] bg-blue-100/70 border-blue-200 outline-none py-1 px-2 text-lg mt-1 rounded-md'
+                                className='border-[1px] font-medium text-[#333] border-[#D6D6D6] outline-none p-[10px] text-lg mt-1 rounded-[10px]'
                                 type="text"
                                 id="numberOfReferralsText"
                                 value={numberOfReferrals}
@@ -188,12 +184,12 @@ const PiebackTabCalculator = () => {
                         <div className="flex flex-col  space-x-4 mb-20">
                             <label
                                 htmlFor="totalReferralVolume"
-                                className="text-lg font-medium text-gray-700"
+                                className="text-xl font-medium"
                             >
                                 Monthly Income from Referral Account
                             </label>
                             <input
-                                className='border-2 font-medium text-[#444] bg-blue-100/70 border-blue-200 outline-none py-1 px-2 text-lg mt-1 rounded-md'
+                                className='border-[1px] font-medium text-[#333] border-[#D6D6D6] outline-none p-[10px] text-lg mt-1 rounded-[10px]'
                                 type="text"
                                 id="totalReferralVolume"
                                 readOnly
@@ -204,7 +200,7 @@ const PiebackTabCalculator = () => {
                         <div className="flex flex-col space-x-4 mb-3">
                             <label
                                 htmlFor="businessVolume"
-                                className="text-lg font-medium text-gray-700"
+                                className="text-xl font-medium"
                             >
                                 Business Volume
                             </label>
@@ -217,7 +213,7 @@ const PiebackTabCalculator = () => {
                                 onChange={handleBusinessVolumeRangeChange}
                             />
                             <input
-                                className='border-2 font-medium text-[#444] bg-blue-100/70 border-blue-200 outline-none py-1 px-2 text-lg mt-1 rounded-md'
+                                className='border-[1px] font-medium text-[#333] border-[#D6D6D6] outline-none p-[10px] text-lg mt-1 rounded-[10px]'
                                 type="text"
                                 id="businessVolumeText"
                                 value={businessVolume}
@@ -228,7 +224,7 @@ const PiebackTabCalculator = () => {
                         <div className="flex flex-col space-x-4 mb-3">
                             <label
                                 htmlFor="personMonthlyFee"
-                                className="text-lg font-medium text-gray-700"
+                                className="text-xl font-medium"
                             >
                                 PiePay Monthly Fee
                             </label>
@@ -241,7 +237,7 @@ const PiebackTabCalculator = () => {
                                 onChange={handlePersonMonthlyFeeRangeChange}
                             />
                             <input
-                                className='border-2 font-medium text-[#444] bg-blue-100/70 border-blue-200 outline-none py-1 px-2 text-lg mt-1 rounded-md'
+                                className='border-[1px] font-medium text-[#333] border-[#D6D6D6] outline-none p-[10px] text-lg mt-1 rounded-[10px]'
                                 type="text"
                                 id="personMonthlyFeeText"
                                 value={personMonthlyFee}
@@ -252,12 +248,12 @@ const PiebackTabCalculator = () => {
                         <div className="flex flex-col space-x-4">
                             <label
                                 htmlFor="currentMonthlyFee"
-                                className="text-lg font-medium text-gray-700"
+                                className="text-xl font-medium"
                             >
                                 Current Monthly Fees
                             </label>
                             <input
-                                className='border-2 font-medium text-[#444] bg-blue-100/70 border-blue-200 outline-none py-1 px-2 text-lg mt-1 rounded-md'
+                                className='border-[1px] font-medium text-[#333] border-[#D6D6D6] outline-none p-[10px] text-lg mt-1 rounded-[10px]'
                                 type="text"
                                 id="currentMonthlyFee"
                                 value={currentMonthlyFee}
@@ -265,10 +261,10 @@ const PiebackTabCalculator = () => {
                             />
                         </div>
                     </div>
-                    <div className='w-[38%] pl-8 pr-4'>
+                    <div className='w-[50%] pl-8 pr-4'>
                         {/* Donut Chart */}
                         <div className="flex justify-between mb-4">
-                            <label className="text-xl">Select Period:</label>
+                            <label className="text-xl font-medium">Select Period</label>
                             <select
                                 className="bg-slate-50 text-xs ml-2 translate-y-[5px] rounded-lg outline-none border-2 border-blue-300"
                                 onChange={handleDropdownChange}
@@ -278,28 +274,26 @@ const PiebackTabCalculator = () => {
                                 <option value="Yearly">Yearly</option>
                             </select>
                         </div>
-                        <div>
-                            <Doughnut data={data} options={options} />
-                        </div>
+                        
                         {/* Savings with Piepay */}
-                        <div className="bg-blue-100/90 p-6 rounded-2xl my-5 shadow-lg shadow-blue-100 border-2 border-blue-300/20">
+                        <div className="bg-[#4686BC] p-[10px] rounded-[20px] my-5 shadow-lg shadow-blue-100 text-white flex flex-col justify-center items-center">
                             <div className="flex">
-                                <label htmlFor="monthlyYearlysavings" className="text-2xl font-semibold text-[#222]">Savings with PiePay</label>
+                                <h3 className="text-[32px] font-bold">Savings with PiePay</h3>
                             </div>
-                            <h3 className="text-6xl text-blue-800/70 font-bold mt-2 ml-1 font-mono">{savings.toFixed(2)}$</h3>
+                            <h3 className="text-[50px] font-bold">{savings.toFixed(2)}$</h3>
                         </div>
                         {/* Earnings with Getpie.io */}
-                        <div className="bg-green-100/90 p-6 rounded-2xl shadow-lg shadow-green-100 border-2 border-green-300/20">
+                        <div className="bg-[#62956A] p-[10px] rounded-[20px] shadow-lg shadow-green-100 text-white flex flex-col justify-center items-center">
                             <div className="flex">
-                                <label htmlFor="monthlyYearlysavings" className="text-2xl font-semibold text-[#222]">Earnings with Getpie</label>
+                                <h3 className="text-[32px] font-bold">Earnings with Getpie</h3>
                             </div>
-                            <h3 className="text-6xl text-green-800/70 font-bold mt-2 ml-1 font-mono">{earnings.toFixed(2)}$</h3>
+                            <h3 className="text-[50px] font-bold">{earnings.toFixed(2)}$</h3>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="mt-8 flex gap-3 justify-center">
-                <button onClick={()=> navigate(`/schedule-a-meeting/${"wsw"}/piebackCalculator`)} className="cursor-pointer flex items-center gap-2 bg-blue-700/70 border-2 border-blue-500 py-2.5 shadow-lg shadow-blue-300/60 px-5 rounded-xl"><div><LuPhoneCall color="#f4f4f4" size={19} /></div><span className="text-lg text-[#f4f4f4] font-medium">SCHEDULE AN APPOINTMENT</span></button>
+                <button onClick={() => navigate(`/schedule-a-meeting/${"wsw"}/piebackCalculator`)} className="cursor-pointer flex items-center gap-2 bg-blue-700/70 border-2 border-blue-500 py-2.5 shadow-lg shadow-blue-300/60 px-5 rounded-xl"><div><LuPhoneCall color="#f4f4f4" size={19} /></div><span className="text-lg text-[#f4f4f4] font-medium">SCHEDULE AN APPOINTMENT</span></button>
                 <button onClick={handleCreateBusinessCard} className="cursor-pointer flex items-center gap-2 bg-blue-700/70 border-2 border-blue-500 py-2.5 shadow-lg shadow-blue-300/60 px-5 rounded-xl"><div><IoArrowRedoCircleOutline color="#f4f4f4" size={24} /></div><span className="text-lg text-[#f4f4f4] font-medium">GET THIS ANALYSIS QR + LINK </span></button>
                 <button onClick={handleCreateBusinessCard} className="cursor-pointer flex items-center gap-2 bg-blue-700/70 border-2 border-blue-500 py-2.5 shadow-lg shadow-blue-300/60 px-5 rounded-xl"><div><MdOutlineFileDownload color="#f4f4f4" size={23} /></div><span className="text-lg text-[#f4f4f4] font-medium">DOWNLOAD THIS ANALYSIS</span></button>
             </div>
